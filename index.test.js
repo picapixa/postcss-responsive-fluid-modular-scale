668403031a96a23c45db4e1a26315d274bd5d0d7
Raffy Alcoriza <raffy@kalibrr.com>
2020-10-27 16:25:08 +0800

    Add more tests, lower test cov threshold for now

--- index.test.js
+++ index.test.js
@@ -8,14 +8,52 @@ async function run (input, output, opts = { }) {
   expect(result.warnings()).toHaveLength(0)
 }
 
-const testInput = `.test-h1 {
-  @ms 0;
-}`
-
-const testOutput = `.test-h1 {
-  font-size: 16px;
-}`
-
 it('adds the font sizes properly on the stylesheet', async () => {
-  await run(testInput, testOutput, {})
+  const testInput = `.test-h1 {
+    @ms 0;
+  }`
+  
+  const testOutput = `.test-h1 {
+    font-size: 16px;
+  }`
+
+ await run(testInput, testOutput, {})
+})
+
+it('fails the build when the modular scale key does not exist in the config', async() => {
+  const testInput = `.test-h1 {
+    @ms 0;
+  }`
+  
+  const options = {
+    scales: {
+      base: 0,
+    }
+  };
+
+  const process = postcss([plugin(options)]).process(testInput, { from: undefined })
+  await expect(process).rejects.toThrow()
+})
+
+it('accounts for multiple breakpoints', async () => {
+  const testInput = `.test-body {
+    @ms 0;
+  }`
+  
+  const options = {
+    fontSize: 14,
+    sm: {
+      min: "768px",
+      fontSize: 16
+    }
+  }
+
+  const testOutput = `.test-body {
+    font-size: 14px;
+  }@media screen and (min-width: 768px) {.test-body {
+        font-size: 16px;
+    }
+}`
+
+  await run(testInput, testOutput, options)
 })
