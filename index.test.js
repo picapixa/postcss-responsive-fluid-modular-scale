60e31c3dfe43ac9dacaa44afe0ca8db4c9cf15f0
Raffy Alcoriza <raffy@kalibrr.com>
2020-10-27 15:59:48 +0800

    Fix test cases

--- index.test.js
+++ index.test.js
@@ -8,13 +8,13 @@ async function run (input, output, opts = { }) {
   expect(result.warnings()).toHaveLength(0)
 }
 
-const testInput = ```.test-h1 {
+const testInput = `.test-h1 {
   @ms 0;
-}```
+}`
 
-const testOutput = ```.test-h1 {
+const testOutput = `.test-h1 {
   font-size: 16px;
-}```
+}`
 
 it('adds the font sizes properly on the stylesheet', async () => {
   await run(testInput, testOutput, {})
